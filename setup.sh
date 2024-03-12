e90dd8d4e1f47a38ec2cfbe98a28ad12b93c0601
TMTCo <149132955+TMTCo@users.noreply.github.com>
2024-03-12 15:16:19 +1100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# use this script to install a desktop gui on ubuntu server 22.04
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install slim -y
+sudo apt install ubuntu-desktop -y
+sudo reboot
